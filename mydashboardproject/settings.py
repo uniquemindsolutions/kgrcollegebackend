6efbabfa75d5487b3c6d6511e20ac01d31ec3dda
Chandra.Vamshikrishna <89682888+vamshikrishnachandra@users.noreply.github.com>
2025-02-17 17:50:01 +0530

    Update settings.py

--- mydashboardproject/settings.py
+++ mydashboardproject/settings.py
@@ -170,7 +170,7 @@ EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST_USER = 'upender@uniquemindsolutions.com'
 EMAIL_HOST_PASSWORD = 'S@m$a@321!12'  # App-specific password
-# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 SESSION_COOKIE_SECURE = True  # if using HTTPS
 CSRF_COOKIE_SECURE = True  # if using HTTPS
